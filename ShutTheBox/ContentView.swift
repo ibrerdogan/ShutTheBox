7d6e24ac55e313499b3c2818254d6262591f9c11
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-09-01 20:36:10 +0300

    Initial Commit

---
+++ ShutTheBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShutTheBox
+//
+//  Created by İbrahim Erdogan on 1.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShutTheBox/ShutTheBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutTheBoxApp.swift
+//  ShutTheBox
+//
+//  Created by İbrahim Erdogan on 1.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShutTheBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
